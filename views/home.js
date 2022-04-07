84a8cb0a02ab332bae91b92024fcd6a384babf8e
Djalmir <djalmir_miodutzki@hotmail.com>
2022-04-07 03:17:58 -0300

    z-if, z-else

--- views/home.js
+++ views/home.js
@@ -7,6 +7,7 @@ template.innerHTML = /*html*/`
 		section {
 			background: var(--dark-bg3);
 			margin: 20px;
+			border-radius: .3rem;
 		}
 
 		input {
@@ -14,19 +15,44 @@ template.innerHTML = /*html*/`
 		}
   </style>
 
-	<h1>Zion.JS</h1>
+	<h1 style="text-align:center;">ZionJS</h1>
   <section>
+		<!-- Testing Two way data binding and method calls on events -->
     <input type="text" z-model="user.name" placeholder="Nome"/>
     <input type="text" z-model="user.age" placeholder="Idade"/>
 		<button z-onclick="()=>{this.user.name='';this.user.age=''}">Clear</button>
 		<button z-onclick="change">name=Djalmir; age=32;</button>
   </section>
 
+	<section style="display: flex; gap: 30px; align-items: center;">
+		<!-- Testing conditional rendering -->
+		<div>
+			<b>Show image?</b><br/>
+			<label>
+				<input type="radio" value="false" z-model="showImage">
+				No
+			</label><br/>
+			<label>
+				<input type="radio" value="true" z-model="showImage">
+				Yes
+			</label>
+		</div>
+		<img z-if="showImage" src="../img.png" style="height:53px;margin-left:20px;">
+		<b z-else>This text shows up when the image isn't visible!</b>
+	</section>
+
 	<section>
-		<input type="text" z-model="user.name" placeholder="Nome"/>
-    <input type="text" z-model="user.age" placeholder="Idade"/>
-		<button z-onclick="()=>{this.user.name='';this.user.age=''}">Clear</button>
-		<button z-onclick="change">name=Djalmir; age=32;</button>
+		<!-- Testing conditional rendering without z-model -->
+		<p>To make the picture below show and hide, change "showImage2" variable value throught the console.</p>
+		<pre>
+			<code>
+let App = app.firstElementChild;
+App.showImage2 = false;
+			</code>
+		</pre>
+		<br/>
+		<img z-if="showImage2" src="../img.png" style="height:53px;margin-left:20px;">
+		<b z-else>This text shows up when the image isn't visible!</b>
 	</section>
  </zion-component>
 `
@@ -39,14 +65,17 @@ export default class Home extends HTMLElement {
 
 		/*Watch*/
 		this.watch = {
-			'user.name': () => {
-				console.log('user name changed to', this.user.name)
-				if (this.user.name == 'Zama')
-					alert('lol')
-			},
-			'user.age': () => {
-				console.log('user age changed to', this.user.age)
-			}
+			// 'user.name': () => {
+			// 	console.log('user name changed to', this.user.name)
+			// 	if (this.user.name == 'Zama')
+			// 		alert('lol')
+			// },
+			// 'user.age': () => {
+			// 	console.log('user age changed to', this.user.age)
+			// }
+			// 'showImage': () => {
+			// 	console.log('this.showImage', this.showImage)
+			// }
 		}
 		/* ******** */
 
@@ -55,6 +84,9 @@ export default class Home extends HTMLElement {
 			name: 'Hosama',
 			age: 28
 		}
+
+		this.showImage = false
+		this.showImage2 = true
 		/* ******* */
 
 		/* Methods */
--- zion.js
+++ zion.js
@@ -21,8 +21,8 @@ const ZION = (self) => {
 
 	//Setting HTML events method callings (z-onclick = onclick, etc)
 	eventDirectives.map((eDirective) => {
-		let eventElements = Array.from(view.querySelectorAll(`[${ eDirective }]`))
-		eventElements.map((el) => {
+		let elements = Array.from(view.querySelectorAll(`[${ eDirective }]`))
+		elements.map((el) => {
 			let method = el.getAttribute(eDirective).replaceAll('this', 'self')
 			//The line below would look like: el.onclick = someMethod
 			el[`${ eDirective.replace(/z-/gi, '') }`] = self[method] || eval(method)
@@ -36,27 +36,63 @@ const ZION = (self) => {
 		zModeledKeys.push(binded)
 		let arr = binded.split('.')
 		let prop = arr[arr.length - 1]
-		let scope = eval('self.' + [...arr.splice(0, arr.length - 1)].join('.'))
+		let scope
+		if (arr.length > 1)
+			scope = eval('self.' + [...arr.splice(0, arr.length - 1)].join('.'))
+		else
+			scope = self
 
 		//fills the element with the property value once the view is loaded
-		el.value = scope[prop]
+		if (el.type != 'radio' && el.type != 'checkbox')
+			el.value = scope[prop]
+		else if (el.type == 'radio' || el.type == 'checkbox')
+			el.checked = scope[prop] == eval(el.value)
+		else
+			el.textContent = scope[prop]
 
 		//update property on input key up event
 		el.onkeyup = () => {
 			scope[prop] = el.value
 		}
+		//update property on change
+		el.onchange = () => {
+			if ((el.type != 'radio' && el.type != 'checkbox') || el.checked) {
+				scope[prop] = el.value
+			}
+		}
+
+		let zIfElements = Array.from(view.querySelectorAll('[z-if]')).filter(zEl => zEl.getAttribute('z-if') == el.getAttribute('z-model'))
+		let currVal = scope[prop]
+
+		//Show or hide z-if elements accordingly
+		zIfElements.map((zEl) => {
+			//TODO: UPDATE THIS BLOCK!!!
+			zEl.style.display = eval(currVal) ? 'unset' : 'none'
+			if (zEl.nextElementSibling) {
+				if (zEl.nextElementSibling.hasAttribute('z-else'))
+					zEl.nextElementSibling.style.display = eval(currVal) ? 'none' : 'unset'
+			}
+		})
+
 
 		//Set the property getter and setter
 		Object.defineProperty(scope, prop, {
 			get: () => {
-				return el.value
+				return currVal
 			},
 			set: (newValue) => {
+				if (typeof newValue === 'undefined')
+					currVal = eval(newValue)
+				else
+					currVal = newValue
+
 				let zElements = elements.filter(e => e.getAttribute('z-model') == el.getAttribute('z-model'))
 				//Once the property receives a new value, update all elements binded to it
 				zElements.map((zEl) => {
-					if (zEl.type)
+					if (zEl.type != 'radio' && zEl.type != 'checkbox')
 						zEl.value = newValue
+					else if (zEl.type == 'radio' || zEl.type == 'checkbox')
+						zEl.checked = eval(newValue) == eval(zEl.value)
 					else
 						zEl.textContent = newValue
 				})
@@ -66,6 +102,16 @@ const ZION = (self) => {
 				if (Object.keys(watch).includes(binded)) {
 					watch[binded]()
 				}
+
+				//if element has z-if directive proccess it now				
+				zIfElements.map((zEl) => {
+					//TODO: UPDATE THIS BLOCK!!!
+					zEl.style.display = eval(currVal) ? 'unset' : 'none'
+					if (zEl.nextElementSibling) {
+						if (zEl.nextElementSibling.hasAttribute('z-else'))
+							zEl.nextElementSibling.style.display = eval(currVal) ? 'none' : 'unset'
+					}
+				})
 			}
 		})
 	})
@@ -75,7 +121,11 @@ const ZION = (self) => {
 	Object.keys(watch).filter(key => !zModeledKeys.includes(key)).map((watchKey) => {
 		let arr = watchKey.split('.')
 		let prop = arr[arr.length - 1]
-		let scope = eval('self.' + [...arr.splice(0, arr.length - 1)].join('.'))
+		let scope
+		if (arr.length > 1)
+			scope = eval('self.' + [...arr.splice(0, arr.length - 1)].join('.'))
+		else
+			scope = self
 		watchKeys[watchKey] = scope[prop]
 		Object.defineProperty(scope, prop, {
 			get: () => {
@@ -86,10 +136,52 @@ const ZION = (self) => {
 					watchKeys[watchKey] = newValue
 				else {
 					watchKeys[watchKey] = newValue
-					console.log('watchKeys pushed', watchKeys)
 				}
 				watch[watchKey]()
 			}
 		})
+	})
+
+	//Proccess all z-if directives but the zModeled ones
+	let zIfElements = Array.from(view.querySelectorAll('[z-if]')).filter((el => !zModeledKeys.includes(el.getAttribute('z-if'))))
+	zIfElements.map((zEl) => {
+		let zIf = zEl.getAttribute('z-if')
+		if (typeof self[zIf] !== 'undefined') {
+			let arr = zIf.split('.')
+			let prop = arr[arr.length - 1]
+			let scope
+			if (arr.length > 1)
+				scope = eval('self.' + [...arr.splice(0, arr.length - 1)].join('.'))
+			else
+				scope = self
+
+			zEl.style.display = self[zIf] ? 'unset' : 'none'
+			if (zEl.nextElementSibling) {
+				if (zEl.nextElementSibling.hasAttribute('z-else'))
+					zEl.nextElementSibling.style.display = self[zIf] ? 'none' : 'unset'
+			}
+
+			let currVal = self[zIf]
+			Object.defineProperty(scope, prop, {
+				get: () => {
+					return currVal
+				},
+				set: (newVal) => {
+					currVal = newVal
+					zEl.style.display = newVal ? 'unset' : 'none'//TODO: UPDATE THIS LINE!!!
+					if (zEl.nextElementSibling) {
+						if (zEl.nextElementSibling.hasAttribute('z-else'))
+							zEl.nextElementSibling.style.display = newVal ? 'none' : 'unset'
+					}
+				}
+			})
+		}
+		else {
+			zEl.style.display = eval(zIf) ? 'unset' : 'none'//TODO: UPDATE THIS LINE!!!
+			if (zEl.nextElementSibling) {
+				if (zEl.nextElementSibling.hasAttribute('z-else'))
+					zEl.nextElementSibling.style.display = eval(zIf) ? 'none' : 'unset'
+			}
+		}
 	})
 }
